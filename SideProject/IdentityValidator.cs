2441ec00801a5e5252b37dfb5a6de12750ffc941
Ali <ALi860713@yahoo.com.tw>
2020-05-31 18:51:57 +0800

    extract method and rename

--- SideProject/IdentityValidator.cs
+++ SideProject/IdentityValidator.cs
@@ -3,9 +3,9 @@ using System.Linq;
 
 namespace IdentityCardValidator
 {
-    class IdentityValidator
+    internal class IdentityValidator
     {
-        private readonly Dictionary<char, int> _idMapping = new Dictionary<char, int>
+        private readonly Dictionary<char, int> _firstIdentityCodeMapping = new Dictionary<char, int>
         {
             { 'A', 10 }, { 'B', 11 }, { 'C', 12 }, { 'D', 13 }, { 'E', 14 }, { 'F', 15 }, { 'G', 16 },
             { 'H', 17 }, { 'I', 18 }, { 'J', 19 }, { 'K', 20 }, { 'L', 21 }, { 'M', 22 }, { 'N', 23 },
@@ -30,28 +30,42 @@ namespace IdentityCardValidator
 
         public bool Validate(string input)
         {
-            var inputArray = GetInputArray(input);
+            var inputList = FormatToIdentityCodeList(input);
 
             var sum = 0;
 
             foreach (var (operationNumber, position) in _identityOperationMapping)
             {
-                sum += operationNumber * int.Parse(inputArray[position]);
+                sum += operationNumber * int.Parse(inputList[position]);
             }
 
             return sum % 10 == 0;
         }
 
-        private List<string> GetInputArray(string input)
+        private List<string> FormatToIdentityCodeList(string input)
         {
-            var firstIdentityCodeNumber = _idMapping[input[0]];
+            var firstIdentityCodes = GenerateFirstIdentityCodes(input);
+
+            var identityCodeList = GenerateIdentityCodeList(input, firstIdentityCodes);
+
+            return identityCodeList;
+        }
+
+        private IEnumerable<string> GenerateFirstIdentityCodes(string input)
+        {
+            var firstIdentityCodeNumber = _firstIdentityCodeMapping[input[0]];
+            var tensDigit= (firstIdentityCodeNumber / 10).ToString();
+            var unitsDigit = (firstIdentityCodeNumber % 10).ToString();
+
+            return new List<string> { unitsDigit, tensDigit };
+        }
+
+        private List<string> GenerateIdentityCodeList(string input, IEnumerable<string> firstIdentityNumber)
+        {
             var otherIdentityCode = input.Skip(1).Select(x => x.ToString()).ToList();
 
-            var x1 = (firstIdentityCodeNumber / 10).ToString();
-            var x2 = (firstIdentityCodeNumber % 10).ToString();
-
             otherIdentityCode.Reverse();
-            otherIdentityCode.InsertRange(otherIdentityCode.Count, new List<string> { x2, x1 });
+            otherIdentityCode.InsertRange(otherIdentityCode.Count, firstIdentityNumber);
             otherIdentityCode.Reverse();
 
             return otherIdentityCode;
